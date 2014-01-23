e6e0f95e3a1eee6effa7920658c9e4d60abca071
Alex Rahr <alexrahr@gmail.com>
2014-01-23 19:56:22 +1100

    created new app

---
+++ socialapps/socialapp2/app/models/user_profile.rb
@@ -0,0 +1,5 @@
+class UserProfile < ActiveRecord::Base
+  belongs_to :user
+  validates :profile_pic, :facebook, :twitter, presence: true
+  validates :facebook, :twitter, uniqueness: true
+end
---
+++ socialapps/socialapp2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialapp2::Application.config.secret_key_base = 'cbb2ed445be04fe5984ec7c98648f52dad1971d9f5138f0e5f82fae80ce499cadd31dfc64323666d2830946cb5380a106ed1db9e4bd15a0507357aaaded69447'
